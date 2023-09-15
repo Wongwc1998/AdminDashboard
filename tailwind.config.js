bb1b8796464ce6b0f8732fefead3a57e02e6b071
dylan_huang <wongwc1998@gmail.com>
2023-09-15 22:00:30 +0800

    fix url

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@ export default {
   theme: {
     extend: {
       backgroundImage:{
-        'unsplash': 'url(./unsplash.jpg)'
+        'unsplash': 'url(images/unsplash.jpg)'
       }},
       fontFamily: {
         'norse': ['Norse-Bold']
